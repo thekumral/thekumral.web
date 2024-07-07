d5aae95a8cb2164604415fe0cfbc800a0b1a6c40
Omer Faruk KUMRAL <the.kumral44@gmail.com>
2024-07-07 21:23:09 +0300

    template-Removed and edited

--- thekumral.Web.Dashboard/wwwroot/vendors/layout-handler/layout-handler.js
+++
@@ -1,6 +0,0 @@
-﻿var isFluid = JSON.parse(localStorage.getItem('isFluid'));
-if (isFluid) {
-    var container = document.querySelector('[data-layout]');
-    container.classList.remove('container');
-    container.classList.add('container-fluid');
-}
